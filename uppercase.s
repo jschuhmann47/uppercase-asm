f1229af6973525af0395f14c7e89beac07a42327
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-06-16 16:59:32 -0300

    print something

--- uppercase.s
+++ uppercase.s
@@ -1,8 +1,25 @@
+SYS_EXIT equ 60
+SYS_WRITE equ 1
+STDOUT equ 1
+
+; rdi, rsi, rdx, r10, r8, and r9 for the parameters
+; https://github.com/torvalds/linux/blob/master/arch/x86/entry/syscalls/syscall_64.tbl
+
+section .data
+  msg db "Hello"
+  len equ 5
+
 global _start
 
 section .text
-
 _start:
-  mov rax, 60       ; exit(
-  mov rdi, 0        ;   EXIT_SUCCESS
-  syscall           ; );
+  mov rax, SYS_WRITE
+  mov rdi, STDOUT
+  mov rsi, msg
+  mov rdx, len
+  syscall
+
+  mov rax, SYS_EXIT
+  mov rdi, 0
+  syscall
+
