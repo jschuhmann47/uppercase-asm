ef872c4ef50f01a1568f86a22261c21063caaf4a
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-06-22 15:00:49 -0300

    print a letter

--- uppercase.s
+++ uppercase.s
@@ -5,20 +5,20 @@ STDOUT equ 1
 ; rdi, rsi, rdx, r10, r8, and r9 for the parameters
 ; https://github.com/torvalds/linux/blob/master/arch/x86/entry/syscalls/syscall_64.tbl
 
-section .data
-  msg db "Hello"
-  len equ 5
-
 global _start
 
 section .text
 _start:
+  add rdi, 65
+  push rdi
   mov rax, SYS_WRITE
-  mov rdi, STDOUT
-  mov rsi, msg
-  mov rdx, len
+  mov rdi, STDOUT 
+  mov rsi, rsp
+  mov rdx, 1
   syscall
+  call .exit
 
+.exit:
   mov rax, SYS_EXIT
   mov rdi, 0
   syscall
